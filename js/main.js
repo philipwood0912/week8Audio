2818c15bedc0cd620697fefb372961841ba2dc8d
Wood <a_wood60810@tss-hr420-dm37.fc.ca>
2019-03-04 16:11:13 -0500

    animation

--- js/main.js
+++ js/main.js
@@ -16,7 +16,25 @@
 		audio.currentTime = 0;
 		audio.play();
 
+		// grab the div and animate it
+		let key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+		key.classList.add('playing');
 	}
+
+	function removePlayingClass(event) {
+		//debugger;
+		if (event.propertyName !== "transform") {
+			return;
+		} else {
+			//remove the playing class here
+			console.log('transition is done');
+			event.target.classList.remove('playing');
+		}
+	}
+
+	const keys = Array.from(document.querySelectorAll('.keys'));
+
+	keys.forEach(key => key.addEventListener('transitionend', removePlayingClass));
 
 	window.addEventListener("keydown", playDrumKitSound);
 })();
